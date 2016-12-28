2fa110ad5d862a756742b5bfd008a0fb4566cc00
hunghk <hunghk.it@gmail.com>
2016-12-28 21:15:04 +0700

    add option to slack relay job

--- app/jobs/slack_job.rb
+++ app/jobs/slack_job.rb
@@ -1,17 +1,15 @@
 class SlackJob < ActiveJob::Base
   
-  def perform(post)
+  def perform(post, options = {})
     # Assemble message
     url = "#{AppSettings['settings.site_url']}#{Rails.application.routes.url_helpers.admin_topic_path(post.topic.id)}"
     title = "New Topic started by #{post.topic.user.name}"
     message = {
       title: "<#{url}|#{post.topic.name}>",
       text: post.body,
-      color: '#eee',
-      topic_id: post.topic.id,
-      keymono_chat_id: post.topic.keymono_chat_id,
-      keymono_message_id: post.topic.keymono_message_id
+      color: '#eee'
     }
+    message.merge!(options) if options.is_a?(::Hash)
 
     # Send Ping
     notifier = Slack::Notifier.new AppSettings['slack.post_url']
--- app/models/post_decorator.rb
+++ app/models/post_decorator.rb
@@ -8,15 +8,21 @@ Post.class_eval do
     # return false unless self.kind == 'first' # Only ping for new topics
 
     # Actually fire the ping, split for private/public topics
+    options = {
+      topic_id: topic.id,
+      keymono_chat_id: topic.keymono_chat_id,
+      keymono_message_id: topic.keymono_message_id
+    }
+
     if self.topic.forum.private?
-      send_ping(self) if AppSettings['slack.ping_private_topics'] == '1'
+      send_ping(self, options) if AppSettings['slack.ping_private_topics'] == '1'
     else
-      send_ping(self) if AppSettings['slack.ping_public_topics'] == '1'
+      send_ping(self, options) if AppSettings['slack.ping_public_topics'] == '1'
     end
   end
 
-  def send_ping(post)
-    SlackJob.perform_later post
+  def send_ping(post, options = {})
+    SlackJob.perform_later post, options
   end
 
 end
